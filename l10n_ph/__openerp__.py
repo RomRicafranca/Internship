aef805cf864439bbe6f8a480832a29fe690562fa
RomRicafranca <romulus@ricafranca.pw>
2016-05-12 16:05:30 -0700

    Added Philippine Accounting Module

---
+++ l10n_ph/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Philippine - Accounting',
+    'version': '0.1',
+    'category': 'Localization/Account Charts',
+    'description': """
+Philippine accounting chart and localization.
+Based on Singapore - Accounting.
+    """,
+    'depends': ['base', 'account'],
+    'demo': [ ],
+    'data': [
+             'l10n_ph_chart.xml',
+             'l10n_ph_chart_tax.xml',
+             'account_chart_template.yml',
+    ],
+    'installable': True,
+}
